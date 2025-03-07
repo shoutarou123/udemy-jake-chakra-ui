4d3c076b9ff5dd72a27532daf20d54e2d3aa8d9b
shotaro <850123.a@gmail.com>
2025-03-07 17:44:36 +0900

    ユーザー一覧取得機能

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,14 +1,30 @@
-import React, { memo } from "react"
-import { Wrap, WrapItem } from "@chakra-ui/react"
+import React, { memo, useEffect } from "react"
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react"
 import { UserCard } from "../organisms/user/UserCard"
+import { useAllUsers } from "../hooks/useAllUsers";
 
 export const UserManagement: React.FC = memo(() => {
+  const { getUsers, loading, users } = useAllUsers();
+
+  useEffect(() => getUsers(), [])
+
+
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-        <WrapItem>
-          <UserCard imageUrl="https://picsum.photos/200" userName="ほげ" fullName="ふるほげ" />
-      </WrapItem>
-      
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard imageUrl="https://picsum.photos/200" userName={user.username} fullName={user.name} />
+            </WrapItem>
+          ))}
+        </Wrap >
+      )}
+
+    </>
   )
 })
