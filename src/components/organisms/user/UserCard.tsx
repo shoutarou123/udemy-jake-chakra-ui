adca409ba9665f39155b388083f0f04a526d5e5c
shotaro <850123.a@gmail.com>
2025-03-07 17:06:22 +0900

    ユーザー一覧画面作成

---
+++ src/components/organisms/user/UserCard.tsx
@@ -0,0 +1,24 @@
+import { Box, Image, Stack, Text } from '@chakra-ui/react';
+import React, { memo, ReactNode } from 'react'
+
+type Props = {
+  imageUrl: string;
+  userName: string;
+  fullName: string;
+}
+
+export const UserCard: React.FC<Props> = memo((props) => {
+  const { imageUrl, userName, fullName} = props;
+  return (
+    <Box w="260px" h="260px" bg="white" borderRadius="10px" shadow="md" p={4} _hover={{ cursor: "pointer", opacity: 0.8 }}>
+
+      <Stack textAlign="center">
+        <Image borderRadius="full" boxSize="160px" src={imageUrl} alt={userName} m="auto" />
+        <Text fontSize="lg" fontWeight="bold">{userName}</Text>
+        <Text fontSize="sm" color="gray">{fullName}</Text>
+      </Stack>
+    </Box>
+  )
+})
+
+// 非活性はdidabledの時とloadingの時に発火
--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,7 +1,14 @@
 import React, { memo } from "react"
+import { Wrap, WrapItem } from "@chakra-ui/react"
+import { UserCard } from "../organisms/user/UserCard"
 
 export const UserManagement: React.FC = memo(() => {
   return (
-    <div>UserManagementページです</div>
+    <Wrap p={{ base: 4, md: 10 }}>
+        <WrapItem>
+          <UserCard imageUrl="https://picsum.photos/200" userName="ほげ" fullName="ふるほげ" />
+      </WrapItem>
+      
+    </Wrap>
   )
 })
