0bfd800399e4d6e5e3a8174ab746ce057ef91cae
shotaro <850123.a@gmail.com>
2025-03-07 23:29:09 +0900

    管理者ユーザーを想定してフラグをcontextに保持してみる

--- src/components/hooks/useAuth.ts
+++ src/components/hooks/useAuth.ts
@@ -17,7 +17,8 @@ export const useAuth = () => {
     setLoading(true);
     axios.get<User>(`https://jsonplaceholder.typicode.com/users/${id}`).then((res) => {
       if (res.data) { // データがtrueであれば
-        setLoginUser(res.data);
+        const isAdmin = res.data.id === 10 ? true : false;
+        setLoginUser({...res.data, isAdmin });
         showMessge({title: "ログインに成功しました", status: "success"})
         navigate("/home"); // home画面に遷移
       } else {
--- src/components/providers/LoginUserProvider.tsx
+++ src/components/providers/LoginUserProvider.tsx
@@ -1,17 +1,19 @@
 import { createContext, Dispatch, ReactNode, SetStateAction, useState } from "react";
 import { User } from "../../types/api/user";
 
+type LoginUser = User & { isAdmin: boolean };
+
 
 export type LoginUserContextType = {
-  loginUser: User | null
-  setLoginUser: Dispatch<SetStateAction<User | null>>
+  loginUser: LoginUser | null;
+  setLoginUser: Dispatch<SetStateAction<LoginUser | null>>
 }
 
 export const LoginUserContext = createContext<LoginUserContextType>({} as LoginUserContextType); // asで強制的に型を指定
 
 export const LoginUserProvider = (props: {children: ReactNode}) => {
   const {children} = props;
-  const [loginUser, setLoginUser] = useState<User | null>(null);
+  const [loginUser, setLoginUser] = useState<LoginUser | null>(null);
   return (
     <LoginUserContext.Provider value={{ loginUser, setLoginUser }}>
       {children}
